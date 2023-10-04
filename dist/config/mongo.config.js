81b795e54ad359bf82149d8c80861ae061a58f15
Juan Ochando <juanochando00@gmail.com>
2023-10-04 19:23:59 -0400

    up

--- dist/config/mongo.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-async function dbConnect() {
-    await (0, mongoose_1.connect)(process.env.MONGODB_URL);
-}
-exports.default = dbConnect;
